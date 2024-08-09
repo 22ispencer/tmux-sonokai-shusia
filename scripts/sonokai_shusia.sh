cd45d2637b00318fadf0ef1baf192a5f40128fcc
Isaac Spencer <22ispencer@gmail.com>
2024-08-08 20:26:37 -0500

    updated gray & black

--- scripts/sonokai_shusia.sh
+++ scripts/sonokai_shusia.sh
@@ -8,36 +8,36 @@ source $current_dir/utils.sh
 main()
 {
   # set configuration option variables
-  show_kubernetes_context_label=$(get_tmux_option "@monokai-kubernetes-context-label" "")
-  eks_hide_arn=$(get_tmux_option "@monokai-kubernetes-eks-hide-arn" false)
-  eks_extract_account=$(get_tmux_option "@monokai-kubernetes-eks-extract-account" false)
-  hide_kubernetes_user=$(get_tmux_option "@monokai-kubernetes-hide-user" false)
-  terraform_label=$(get_tmux_option "@monokai-terraform-label" "")
-  show_fahrenheit=$(get_tmux_option "@monokai-show-fahrenheit" true)
-  show_location=$(get_tmux_option "@monokai-show-location" true)
-  fixed_location=$(get_tmux_option "@monokai-fixed-location")
-  show_powerline=$(get_tmux_option "@monokai-show-powerline" true)
-  show_flags=$(get_tmux_option "@monokai-show-flags" false)
-  show_left_icon=$(get_tmux_option "@monokai-show-left-icon" session)
-  show_left_icon_padding=$(get_tmux_option "@monokai-left-icon-padding" 0)
-  show_military=$(get_tmux_option "@monokai-military-time" false)
-  timezone=$(get_tmux_option "@monokai-set-timezone" "")
-  show_timezone=$(get_tmux_option "@monokai-show-timezone" true)
-  show_left_sep=$(get_tmux_option "@monokai-show-left-sep" )
-  show_right_sep=$(get_tmux_option "@monokai-show-right-sep" )
-  show_border_contrast=$(get_tmux_option "@monokai-border-contrast" false)
-  show_day_month=$(get_tmux_option "@monokai-day-month" false)
-  show_refresh=$(get_tmux_option "@monokai-refresh-rate" 5)
-  show_synchronize_panes_label=$(get_tmux_option "@monokai-synchronize-panes-label" "Sync")
-  time_format=$(get_tmux_option "@monokai-time-format" "")
-  show_ssh_session_port=$(get_tmux_option "@monokai-show-ssh-session-port" false)
-  IFS=' ' read -r -a plugins <<< $(get_tmux_option "@monokai-plugins" "network-ping cpu-usage ram-usage")
-  show_empty_plugins=$(get_tmux_option "@monokai-show-empty-plugins" true)
+  show_kubernetes_context_label=$(get_tmux_option "@sonokai-kubernetes-context-label" "")
+  eks_hide_arn=$(get_tmux_option "@sonokai-kubernetes-eks-hide-arn" false)
+  eks_extract_account=$(get_tmux_option "@sonokai-kubernetes-eks-extract-account" false)
+  hide_kubernetes_user=$(get_tmux_option "@sonokai-kubernetes-hide-user" false)
+  terraform_label=$(get_tmux_option "@sonokai-terraform-label" "")
+  show_fahrenheit=$(get_tmux_option "@sonokai-show-fahrenheit" true)
+  show_location=$(get_tmux_option "@sonokai-show-location" true)
+  fixed_location=$(get_tmux_option "@sonokai-fixed-location")
+  show_powerline=$(get_tmux_option "@sonokai-show-powerline" true)
+  show_flags=$(get_tmux_option "@sonokai-show-flags" false)
+  show_left_icon=$(get_tmux_option "@sonokai-show-left-icon" session)
+  show_left_icon_padding=$(get_tmux_option "@sonokai-left-icon-padding" 0)
+  show_military=$(get_tmux_option "@sonokai-military-time" false)
+  timezone=$(get_tmux_option "@sonokai-set-timezone" "")
+  show_timezone=$(get_tmux_option "@sonokai-show-timezone" true)
+  show_left_sep=$(get_tmux_option "@sonokai-show-left-sep" )
+  show_right_sep=$(get_tmux_option "@sonokai-show-right-sep" )
+  show_border_contrast=$(get_tmux_option "@sonokai-border-contrast" false)
+  show_day_month=$(get_tmux_option "@sonokai-day-month" false)
+  show_refresh=$(get_tmux_option "@sonokai-refresh-rate" 5)
+  show_synchronize_panes_label=$(get_tmux_option "@sonokai-synchronize-panes-label" "Sync")
+  time_format=$(get_tmux_option "@sonokai-time-format" "")
+  show_ssh_session_port=$(get_tmux_option "@sonokai-show-ssh-session-port" false)
+  IFS=' ' read -r -a plugins <<< $(get_tmux_option "@sonokai-plugins" "network-ping cpu-usage ram-usage")
+  show_empty_plugins=$(get_tmux_option "@sonokai-show-empty-plugins" true)
 
-  # Monokai Pro Color Pallette
+  # sonokai Pro Color Pallette
   white='#e3e1e4'
-  black='#1a181a'
-  dark_gray='#211f21'
+  black='#2d2a2e'
+  dark_gray='#37343a'
   gray='#605d68'
   red='#f85e84'
   green='#9ecd6f'
@@ -143,7 +143,7 @@ main()
     if case $plugin in custom:*) true;; *) false;; esac; then
       script=${plugin#"custom:"}
       if [[ -x "${current_dir}/${script}" ]]; then
-        IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-custom-plugin-colors" "blue black")
+        IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-custom-plugin-colors" "blue black")
         script="#($current_dir/${script})"
       else
         colors[0]="red"
@@ -152,104 +152,104 @@ main()
       fi
 
     elif [ $plugin = "cwd" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@monokai-cwd-colors" "dark_gray white")
+      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@sonokai-cwd-colors" "dark_gray white")
       tmux set-option -g status-right-length 250
       script="#($current_dir/cwd.sh)"
 
     elif [ $plugin = "fossil" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@monokai-fossil-colors" "green black")
+      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@sonokai-fossil-colors" "green black")
       tmux set-option -g status-right-length 250
       script="#($current_dir/fossil.sh)"
 
     elif [ $plugin = "git" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@monokai-git-colors" "green black")
+      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@sonokai-git-colors" "green black")
       tmux set-option -g status-right-length 250
       script="#($current_dir/git.sh)"
 
     elif [ $plugin = "hg" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@monokai-hg-colors" "green black")
+      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@sonokai-hg-colors" "green black")
       tmux set-option -g status-right-length 250
       script="#($current_dir/hg.sh)"
 
     elif [ $plugin = "battery" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-battery-colors" "red black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-battery-colors" "red black")
       script="#($current_dir/battery.sh)"
 
     elif [ $plugin = "gpu-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-gpu-usage-colors" "red black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-gpu-usage-colors" "red black")
       script="#($current_dir/gpu_usage.sh)"
 
     elif [ $plugin = "gpu-ram-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-gpu-ram-usage-colors" "blue black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-gpu-ram-usage-colors" "blue black")
       script="#($current_dir/gpu_ram_info.sh)"
 
     elif [ $plugin = "gpu-power-draw" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-gpu-power-draw-colors" "green black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-gpu-power-draw-colors" "green black")
       script="#($current_dir/gpu_power.sh)"
 
     elif [ $plugin = "cpu-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-cpu-usage-colors" "orange black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-cpu-usage-colors" "orange black")
       script="#($current_dir/cpu_info.sh)"
 
     elif [ $plugin = "ram-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-ram-usage-colors" "yellow black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-ram-usage-colors" "yellow black")
       script="#($current_dir/ram_info.sh)"
 
     elif [ $plugin = "tmux-ram-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-tmux-ram-usage-colors" "yellow black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-tmux-ram-usage-colors" "yellow black")
       script="#($current_dir/tmux_ram_info.sh)"
 
     elif [ $plugin = "network" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-network-colors" "blue black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-network-colors" "blue black")
       script="#($current_dir/network.sh)"
 
     elif [ $plugin = "network-bandwidth" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-network-bandwidth-colors" "blue black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-network-bandwidth-colors" "blue black")
       tmux set-option -g status-right-length 250
       script="#($current_dir/network_bandwidth.sh)"
 
     elif [ $plugin = "network-ping" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-network-ping-colors" "dark_gray white")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-network-ping-colors" "dark_gray white")
       script="#($current_dir/network_ping.sh)"
 
     elif [ $plugin = "network-vpn" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-network-vpn-colors" "blue black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-network-vpn-colors" "blue black")
       script="#($current_dir/network_vpn.sh)"
 
     elif [ $plugin = "attached-clients" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-attached-clients-colors" "blue black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-attached-clients-colors" "blue black")
       script="#($current_dir/attached_clients.sh)"
 
     elif [ $plugin = "mpc" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-mpc-colors" "green black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-mpc-colors" "green black")
       script="#($current_dir/mpc.sh)"
 
     elif [ $plugin = "spotify-tui" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-spotify-tui-colors" "green black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-spotify-tui-colors" "green black")
       script="#($current_dir/spotify-tui.sh)"
 
     elif [ $plugin = "playerctl" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-playerctl-colors" "green black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-playerctl-colors" "green black")
       script="#($current_dir/playerctl.sh)"
 
     elif [ $plugin = "kubernetes-context" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-kubernetes-context-colors" "blue black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-kubernetes-context-colors" "blue black")
       script="#($current_dir/kubernetes_context.sh $eks_hide_arn $eks_extract_account $hide_kubernetes_user $show_kubernetes_context_label)"
 
     elif [ $plugin = "terraform" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-terraform-colors" "blue black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-terraform-colors" "blue black")
       script="#($current_dir/terraform.sh $terraform_label)"
 
     elif [ $plugin = "continuum" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-continuum-colors" "blue black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@sonokai-continuum-colors" "blue black")
       script="#($current_dir/continuum.sh)"
 
     elif [ $plugin = "weather" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-weather-colors" "orange black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-weather-colors" "orange black")
       script="#($current_dir/weather_wrapper.sh $show_fahrenheit $show_location '$fixed_location')"
 
     elif [ $plugin = "time" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-time-colors" "green black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-time-colors" "green black")
       if [ -n "$time_format" ]; then
         script=${time_format}
       else
@@ -265,11 +265,11 @@ main()
       fi
 
     elif [ $plugin = "synchronize-panes" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-synchronize-panes-colors" "blue black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-synchronize-panes-colors" "blue black")
       script="#($current_dir/synchronize_panes.sh $show_synchronize_panes_label)"
 
     elif [ $plugin = "ssh-session" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-ssh-session-colors" "green black")
+      IFS=' ' read -r -a colors <<< $(get_tmux_option "@sonokai-ssh-session-colors" "green black")
       script="#($current_dir/ssh_session.sh $show_ssh_session_port)"
 
     else
